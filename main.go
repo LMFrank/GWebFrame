87dcd761c6c4edc9e46ea87f2e8e4575cc215a82
Xushuchi <njuxushuchi@163.com>
2020-05-22 22:23:00 +0800

    加入了可扩展的中间件功能

--- main.go
+++ main.go
@@ -1,40 +1,38 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"gwebframe"
 )
 
+func onlyForV2() gwebframe.HandlerFunc {
+	return func(c *gwebframe.Context) {
+		// Start timer
+		t := time.Now()
+		// if a server error occurred
+		//c.Fail(500, "Internal Server Error")
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
+
 func main() {
 	r := gwebframe.New()
-	r.GET("/index", func(c *gwebframe.Context) {
-		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	r.Use(gwebframe.Logger()) // global midlleware
+	r.GET("/", func(c *gwebframe.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello gwebframe</h1>")
 	})
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/", func(c *gwebframe.Context) {
-			c.HTML(http.StatusOK, "<h1>Hello gwebframe</h1>")
-		})
 
-		v1.GET("/hello", func(c *gwebframe.Context) {
-			// /hello?name=admin
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-		})
-	}
 	v2 := r.Group("/v2")
+	v2.Use(onlyForV2()) // v2 group middleware
 	{
 		v2.GET("/hello/:name", func(c *gwebframe.Context) {
-			// /hello/admin
+			// expect /hello/admin
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 		})
-		v2.POST("/login", func(c *gwebframe.Context) {
-			c.JSON(http.StatusOK, gwebframe.H{
-				"username": c.PostForm("username"),
-				"password": c.PostForm("password"),
-			})
-		})
-
 	}
 
 	r.Run(":8000")
