1f2b05f044b87a1fb313eef44aa375fdd01f7282
Xushuchi <njuxushuchi@163.com>
2020-05-23 23:06:49 +0800

    加入了模板渲染功能

--- main.go
+++ main.go
@@ -1,39 +1,51 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"html/template"
 	"net/http"
 	"time"
 
 	"gwebframe"
 )
 
-func onlyForV2() gwebframe.HandlerFunc {
-	return func(c *gwebframe.Context) {
-		// Start timer
-		t := time.Now()
-		// if a server error occurred
-		//c.Fail(500, "Internal Server Error")
-		// Calculate resolution time
-		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
-	}
+type student struct {
+	Name string
+	Age  int8
+}
+
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
 }
 
 func main() {
 	r := gwebframe.New()
-	r.Use(gwebframe.Logger()) // global midlleware
+	r.Use(gwebframe.Logger())
+	r.SetFuncMap(template.FuncMap{
+		"formatAsDate": formatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
+
+	stu1 := &student{Name: "gwebframektutu", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
 	r.GET("/", func(c *gwebframe.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello gwebframe</h1>")
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(c *gwebframe.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", gwebframe.H{
+			"title":  "gwebframe",
+			"stuArr": [2]*student{stu1, stu2},
+		})
 	})
 
-	v2 := r.Group("/v2")
-	v2.Use(onlyForV2()) // v2 group middleware
-	{
-		v2.GET("/hello/:name", func(c *gwebframe.Context) {
-			// expect /hello/admin
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	r.GET("/date", func(c *gwebframe.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", gwebframe.H{
+			"title": "gwebframe",
+			"now":   time.Date(2020, 5, 20, 0, 0, 0, 0, time.UTC),
 		})
-	}
+	})
 
 	r.Run(":8000")
 }
